af775c41166aa7d841fe96bd5d346126e2aa7c10
tqing <itanqing@gmail.com>
2025-03-28 09:38:51 +0800

    微前端-流程为子应用

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,21 @@
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
+//pluginManagement {
+//    repositories {
+//        mavenCentral()
+//        gradlePluginPortal()
+//    }
+//}
 
 rootProject.name = "KotlinNativeTemplate"
+
+pluginManagement {
+    repositories {
+        maven(
+            url = "http://172.16.1.225:8081/repository/maven-public/",
+            action = {
+                isAllowInsecureProtocol = true
+            }
+        )
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
